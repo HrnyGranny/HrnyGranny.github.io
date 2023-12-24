1295c436a4f0e3eeed31f1857eab4a5cec9531a3
HornyGranny <Alvaro.Ruiz12@alu.uclm.es>
2023-12-24 01:40:20 +0100

    2.4.1

---
+++ assets/js/contact.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('#contact_').submit(function (e) {
+        e.preventDefault();
+
+        var nombre = $('#name').val();
+        var correo = $('#email').val();
+        var asunto = $('#subject').val();
+        var mensaje = $('#message').val();
+
+        // Función de validación
+        if (!validarCampos(nombre, correo, asunto, mensaje)) {
+            return;
+        }else{
+            alert('¡Correo enviado!');
+        }
+
+        // Función de validación para los campos del formulario
+        function validarCampos(nombre, correo, asunto, mensaje) {
+            // Validar campos vacíos
+            if (nombre === '' || correo === '' || asunto === '' || mensaje === '') {
+                alert('Por favor, completa todos los campos.');
+                return false;
+            }
+
+            // Validar correo electrónico
+            if (!validarCorreo(correo)) {
+                alert('Por favor, ingresa un correo electrónico válido');
+                return false;
+            }
+            // Validar nombre y mensaje (permitir solo espacios cuando se ingrese texto)
+            if (nombre.trim() === '' || mensaje.trim() === '' || asunto.trim() === '') {
+                alert('Los campos no pueden tener espacios antes de ingresar el texto.');
+                return false;
+            }
+
+
+
+            return true;
+        }
+
+        // Función para validar el formato de correo electrónico
+        function validarCorreo(correo) {
+            // Expresión regular para validar correo electrónico
+            var expresion = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return expresion.test(correo);
+        }
+    });
+});
